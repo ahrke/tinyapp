4500a16ba04aaeacb4c95dec5995f160f9349d49
ahrke <david.duy.minh@gmail.com>
2019-08-05 16:01:38 +0000

    added ejx support. Moved renders into /views folder as separate files

--- express_server.js
+++ express_server.js
@@ -3,18 +3,29 @@ const app = express();
 
 const PORT = 3000;
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my page!\n<br><br><br><img src="https://http.cat/200" />');
+  res.render('index');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get('/whoa', (req,res) => {
-  let randomCatStatus = Math.floor(Math.random() * Math.floor(51)) + 400;
-  res.send('<h1>supposed to have something here...</h1><br><img src="https://http.cat/' + randomCatStatus + '" /><br><h2>did we get anything with ' + randomCatStatus + '?</h2>');
+  res.render('whoa');
+});
+
+app.get('/path/:where', (req, res) => {
+  let arg = res.params.where;
+
 })
 
 app.listen(PORT, () => {
