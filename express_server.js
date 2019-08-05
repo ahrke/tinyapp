76b73fbaa360c2bf44265f6783f738a4fcadadb4
ahrke <david.duy.minh@gmail.com>
2019-08-05 18:04:19 +0000

    added randomStrGenerator to create short url for an address submitted through urls/new form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +24,21 @@ app.get('/urls', (req, res) => {
   res.render('urlsIndex', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let urlShortened = generateRandomString();
+  let templateVars = {
+    urls: urlDatabase
+  }
+  templateVars.urls[urlShortened] = req.body.longURL;
+  console.log(templateVars)
+  res.render("urlsIndex", templateVars);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -39,3 +56,14 @@ app.get('/path/:where', (req, res) => {
 app.listen(PORT, () => {
   console.log('Example app listening to port:',PORT);
 });
+
+const generateRandomString = () => {
+  const ALPHA = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  let urlStr = '';
+
+  for (let i = 0; i < 6; i++) {
+    urlStr += ALPHA[Math.floor(Math.random() * ALPHA.length)];
+  }
+
+  return urlStr;
+}
